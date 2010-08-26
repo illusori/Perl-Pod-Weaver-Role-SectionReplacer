c1280371627a31e3ba81019a4029dc4dbea086e0
Sam Graham <git@illusori.co.uk>
2010-08-26 13:59:25 +0100

    Added documentation for role methods.

--- lib/Pod/Weaver/Role/SectionReplacer.pm
+++ lib/Pod/Weaver/Role/SectionReplacer.pm
@@ -94,7 +94,7 @@ Pod::Weaver::Role::SectionReplacer - a Pod::Weaver section that will replace its
 
 This role is used by plugins that will find an existing section in the input
 document.
-It will prune the existing section from the input document and make it
+It will prune the first existing section from the input document and make it
 available under C<original_section> method:
 
   $section_plugin->original_section();
@@ -102,6 +102,12 @@ available under C<original_section> method:
 The plugin could then choose to keep the original, by inserting it
 into the document again, or to write something new instead, or some
 combination of the two.
+
+=head2 REQUIRED METHODS
+
+=over
+
+=item B<< $plugin->default_section_name() >>
 
 The plugin must provide a method, C<default_section_name> which will return
 the default name of the section, as used in the =head1 line, this is
@@ -116,6 +122,14 @@ the end-user to configure the section name in their weaver.ini, eg:
   [ReplaceLegal]
   section_name = MY CUSTOMIZED LICENSE AND COPYRIGHT HEADING
 
+=back
+
+=head2 OPTIONAL METHODS
+
+=over
+
+=item B<< $plugin->default_section_aliases >>
+
 The plugin may also provide a C<default_section_aliases> method, which
 should return an arrayref of alternative section names to match.
 Like C<section_name> this allows the end-user to override the default
@@ -127,6 +141,37 @@ section aliases:
   section_alias = COPYRIGHT AND LICENSE
   section_alias = LICENCE AND COPYRIGHT
   section_alias = COPYRIGHT AND LICENCE
+
+=back
+
+=head2 INTERNAL METHODS
+
+These methods are mostly internal to the role, but if you're also using
+them in your plugin, you will need reconcile the return values.
+
+=over
+
+=item B<< $plugin->mvp_aliases() >>
+
+Tells L<Config::MVP> that C<section_alias> is a synonym for C<section_aliases>
+in the weaver.ini for plugins that use this role.
+
+=item B<< $plugin->mvp_multivalue_args() >>
+
+Tells L<Config::MVP>> that C<section_aliases> always takes multiple-values
+and should be stored as an arrayref.
+
+=item B<< $plugin->transform_document() >>
+
+L<Pod::Weaver:Role::SectionReplacer>> implements the role
+L<Pod::Weaver::Role::Transformer>>, and as such it provides its own
+C<< $plugin->tranform_document() >> method in order to prune the original
+section from the input document before any further weaving is done.
+
+If your plugin wishes to implement a C<< transform_document() >> of its
+own, you will need to reconcile the two.
+
+=back
 
 =head1 AUTHOR
 
